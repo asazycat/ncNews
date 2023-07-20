08724eba0cfca3847cd647b9dde9df6fb340d527
asazycat <asazycat@example.com>
2023-07-20 11:07:34 +0100

    list of comments for each article

---
+++ components/EachComment.jsx
@@ -0,0 +1,32 @@
+function eachComment (props)
+{
+   const {eachComment} = props
+
+    return (
+        <>
+        
+        <li key={eachComment.comment_id} className='eachComment'>
+
+            <h1 className="commentTitle">Username: {eachComment.author}</h1>
+            
+            <p>{eachComment.body}</p>
+            <p>Date: {eachComment.created_at}</p>
+            <p>Votes: {eachComment.votes}</p>
+            </li>
+        
+        </>
+    
+        )
+    
+
+
+
+
+
+}
+
+
+
+
+
+export default eachComment
