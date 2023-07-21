bb42dfabd4ee1ac290c2191419cf529e3bbe9e3d
asazycat <asazycat@example.com>
2023-07-21 10:00:38 +0100

    KaterineRules wanted Optimistic Rendering so I did just that

--- components/Upvotes.jsx
+++ components/Upvotes.jsx
@@ -4,33 +4,78 @@
 import {useEffect, useState} from 'react'
 import {upvoteArticle} from '../util.js/util'
 
+// function Upvotes(props) {
+//     const {id,setVotes,votes} = props
+//    const [newVote, setNewVotes] = useState(votes)
+//    const [state,setState] = useState(false)
+
+
+//     useEffect(()=> {
+       
+       
+       
+       
+       
+//         if(state == true) { 
+//         upvoteArticle(id).then((newVoteCount)=> {
+//            const {votes} = newVoteCount
+//             setVotes(votes)
+//            setNewVotes(votes)
+//            setState(false)
+           
+//         })
+
+// }},[state])
+
+
+
+// return (
+
+//  <p><button onClick={()=> setState(true)} >Upvote: </button>  {newVote}</p>
+    
+// )
+
+// }
+
+
+
+
+
+
 function Upvotes(props) {
     const {id,setVotes,votes} = props
    const [newVote, setNewVotes] = useState(votes)
-   const [state,setState] = useState(false)
-
-
+   const [firstEffect,setFirstEffect] = useState(false)
+   
+   
     useEffect(()=> {
-        if(state == true) { 
-        upvoteArticle(id).then((newVoteCount)=> {
-           const {votes} = newVoteCount
-            setVotes(votes)
-           setNewVotes(votes)
-           setState(false)
-           
-        })
+       
+      
+    if (firstEffect)
+    {
+          setVotes(newVote)
+          
+         
+          
+       upvoteArticle(id)
+         }
+          
+        }   
+        ,[newVote])
 
-}},[state])
-
-
+   
 
 return (
 
- <p><button onClick={()=> setState(true)} >Upvote: </button>  {newVote}</p>
+ <p><button onClick={()=> {setNewVotes(newVote+1) 
+    setFirstEffect(true)}} >Upvote: </button>  {newVote}</p>
     
 )
 
 }
+
+
+
 
 
 
